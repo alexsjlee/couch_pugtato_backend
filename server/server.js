1962b3561f347951a1615bf90a94465ff5bbb956
alexsjlee <sjlee89@gmail.com>
2017-07-21 12:52:39 -0700

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -7,6 +8,7 @@ var app = express();
 const port = process.env.PORT || 3090;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 router(app);
 
